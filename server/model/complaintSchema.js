40db17b66c991cfdc5836df82f3bd758992ef884
AmoghKharche <amoghkharche@gmail.com>
2023-03-21 10:24:20 +0530

    Added timezone

--- server/model/complaintSchema.js
+++ server/model/complaintSchema.js
@@ -1,16 +1,31 @@
 const mongoose = require("mongoose");
-
-const uid = crypto.getRandomValues();
+const ShortUniqueId = require("short-unique-id");
+const uid = new ShortUniqueId({ length: 6 });
+const moment = require("moment-timezone");
 
 const complaintSchema = new mongoose.Schema(
   {
-    complaint: { type: String, required: true },
-    name: String,
-    ward: Number,
-    tag: String,
-    uid: uid().String,
-  },
-  { timestamps: true }
+    complaint: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    ward: { type: Number, required: true },
+    tag: { type: String, required: true },
+    ticketid: { type: String, default: uid(), min: 5, unique: true },
+    createdAt: {
+      type: Date,
+      default: () => moment().tz("Asia/Calcutta").format(),
+    },
+  }
+  // { timestamps: true }
 );
 
 module.exports = mongoose.model("Complaint", complaintSchema);
