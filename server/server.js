e1c08dfd9030cb50d5db1973a806e6dd7e918075
AmoghKharche <amoghkharche@gmail.com>
2023-05-04 22:50:48 +0530

    deployment

--- server/server.js
+++ server/server.js
@@ -7,14 +7,14 @@ const morgan = require("morgan");
 dotenv.config({ path: "./config.env" });
 const cookieParser = require("cookie-parser");
 const fileUpload = require("express-fileupload");
-
+const path = require("path");
 require("./db/conn");
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors());
 app.use(cookieParser());
 app.use(fileUpload());
-
+app.use(express.static(path.join(__dirname + "/public")));
 app.use(require("./router/auth"));
 
 // app.get('/about', middleware, (req, res)=>{
